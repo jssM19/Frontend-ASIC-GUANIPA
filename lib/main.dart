1d104654fac4850e2fc179320ccff4184da03065
Erik Mota Galindo <kenshimota@gmail.com>
2026-01-14 12:08:46 -0300

    making deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
